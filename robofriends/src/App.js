1c8c10ef0483d7fc98315dfff414ea624188f8aa
Manuel Novoa <novoamanuel@ymail.com>
2018-12-02 14:41:52 -0600

    Landing Page
    Landing Page for RoboFriends is not finished. Search bar will filter and display what the user inputs.
    Robots are hardocoded at the moment but will fix next push update.

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -5,14 +5,38 @@ import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 import {robots} from './components/Robots';
 
+
+
 class App extends Component {
+
+  constructor(){
+    // STATE
+    super();
+    this.state={
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value});
+  }
+
   render() {
+    // Filter Search
+    const filterRobots = this.state.robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
+
+
+
+
     return (
       
       <div className="App tc">
-        <h1>Robo Friends</h1>
-        <SearchBox/>
-        <CardList robots={robots}/>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filterRobots}/>
       </div>
     );
   }
--- robofriends/src/components/SearchBox.jsx
+++ robofriends/src/components/SearchBox.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-const SearchBox = () =>{
+const SearchBox = ({searchField, searchChange}) =>{
     return (
         <div className='pd2'>
             <input
                 className='pa2 ba b--green bg-lightest-blue' 
                 tyoe='search' 
                 placeholder='Search Robots'
+                onChange={searchChange}
             />
         </div>
     );
